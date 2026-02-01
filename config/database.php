b07e060db870ca5f067d70a367be6d3268519be2
CoW08 <callofwar0816@gmail.com>
2026-02-02 01:58:07 +0800

    asd

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'prop_custodian_db';
+    private $host = '127.0.0.1';
+    private $db_name = 'systems';
     private $db_port = "3306"; 
-    private $username = 'prop_custodian_db';
-    private $password = '123';
+    private $username = 'root';
+    private $password = 'root';
     public $conn;
 
     public function getConnection() {
